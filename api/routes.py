b11c514ab3ce5f8bf36b6e4d2ced52433dc167db
joel <joel.vanegmond@ucalgary.ca>
2019-02-07 21:49:29 -0700

    - Added dummy responses for all endpoints - Added pylint to dev dependencies (maybe I shouldnt add that kind of thing)

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,5 @@
 from api import app
-from flask import request
+from flask import request, jsonify
 
 
 # Gets the list of files the tracker knows about
@@ -9,10 +9,10 @@ from flask import request
 # Returns a JSON blob of the form:
 '''
 {
-    "success" : "<true/false>",
+    "success" : <true/false>,
     "files" : [
         {
-            "id" : "<file id>",
+            "id" : <file id>,
             "name" : "<file's name>",
             "hash" : "<full file hash>"
         },
@@ -24,14 +24,36 @@ from flask import request
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "false",
+    "success" : false,
     "error" : "<error reason>"
 }
 '''
 @app.route('/file_list', methods=['GET'])
 def get_file_list():
     # pull the list of file ids and names from db and convert to json
-    return "this will be a list of available files"
+
+    dummyResponse = {
+        "success" : "true",
+        "files" : [
+            {
+                "id" : 1,
+                "name" : "joel",
+                "hash" : "alksjhf98ufyah9"
+            },
+            {
+                "id" : 2,
+                "name" : "is",
+                "hash" : "0f9ua40jf0j0934j0w39j09"
+            },
+            {
+                "id" : 3,
+                "name" : "great",
+                "hash" : "j9ra09jrega546df684fgad648agdf"
+            }
+        ]
+    }
+
+    return jsonify(dummyResponse)
 
 
 # TODO: consider replacing the id with the file's hash or to a json blob input to make it tracker independent
@@ -44,7 +66,7 @@ def get_file_list():
 # Returns a JSON blob of the form:
 '''
 {
-    "success" : "<true/false>",
+    "success" : <true/false>,
     "name" : "<file name>",
     "file_hash" : "<hash of the full file>",
     "peers" : [
@@ -53,9 +75,10 @@ def get_file_list():
     ],
     "chunks" : [
         { 
-            "name" : "<chunk filename",
+            "name" : "<chunk filename>",
             "hash" : "<hash of chunk>"
-        }
+        },
+        ...
     ]
 }
 '''
@@ -63,14 +86,40 @@ def get_file_list():
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "false",
+    "success" : false,
     "error" : "<error reason>"
 }
 '''
 @app.route('/file/<id>', methods=['GET'])
 def get_file(id):
     # pull the file metadata from the db (name, list of peers, list of chunks, etc)
-    return "this will be file {}'s metadata, if it exists".format(id)
+
+    dummyResponse = {
+        "success" : True,
+        "name" : "sick nasty file with id {}".format(id),
+        "file_hash" : "arfihu9f87h49h72f89hyufaf9h78af439h878h9f739h8a8h7",
+        "peers" : [
+            { "ip" : "192.168.0.1" },
+            { "ip" : "192.168.0.2" },
+            { "ip" : "192.168.0.3" }
+        ],
+        "chunks" : [
+            { 
+                "name" : "avengers_1.mp4",
+                "hash" : "asdljiksfadlkhjsadflkjhdsaflkjhsdaflkj"
+            },
+            { 
+                "name" : "avengers_2.mp4",
+                "hash" : "assadf5f4sd54asfd456"
+            },
+            { 
+                "name" : "avengers_3.mp4",
+                "hash" : "fdgh68fhg64d6d4f5gh3fg6h5d4687"
+            }
+        ]
+    }
+
+    return jsonify(dummyResponse)
 
 
 # Gets the list of other trackers the tracker knows about
@@ -80,9 +129,9 @@ def get_file(id):
 # Returns a JSON blob of the form:
 '''
 {
-    "success" : "<true/false>",
+    "success" : <true/false>,
     "trackers" : [
-        { "name" : "<tracker name>", ip : "<tracker's ip"},
+        { "name" : "<tracker name>", "ip" : "<tracker's ip>"},
         ...
     ]
 }
@@ -91,14 +140,24 @@ def get_file(id):
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "false",
+    "success" : false,
     "error" : "<error reason>"
 }
 '''
 @app.route('/tracker_list', methods=['GET'])
 def get_tracker_list():
     # pull the list of other trackers from the db
-    return "this will be the list of trackers"
+
+    dummyResponse = {
+        "success" : True,
+        "trackers" : [
+            { "name" : "the p2p bay", "ip" : "1.1.1.1"},
+            { "name" : "the p2p bay", "ip" : "1.1.1.2"},
+            { "name" : "the p2p bay", "ip" : "1.1.1.3"}
+        ]
+    }
+
+    return jsonify(dummyResponse)
 
 
 # expects JSON blob of metadata about the file
@@ -111,7 +170,10 @@ def get_tracker_list():
     "name" : "<file name>",
     "full_hash" : "<hash of full file>",
     "chunks" : [
-        { "name" : "<chunk filename>", "hash" : "<hash of chunk>"},
+        { 
+            "name" : "<chunk filename>", 
+            "hash" : "<hash of chunk>"
+        },
         ...
     ],
     "guid" : "<client's guid>"
@@ -121,26 +183,33 @@ def get_tracker_list():
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "<true/false>",
-    "file_id" : "<the existing id if the tracker already has it, or the new one if it didnt>"
+    "success" : <true/false>,
+    "file_id" : "<the existing id if the tracker already has it, or the new one if it didnt>",
+    "guid" : "<echoed guid if you had one already, otherwise your newly assigned one"
 }
 '''
 # --- ON ERROR ---
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "false",
+    "success" : false,
     "error" : "<error reason>"
 }
 '''
 @app.route('/add_file', methods=['POST'])
 def add_file():
-    if(not request.json or not "title" in request.json):
+    if(not request.json):
         return "error: request is not json"
 
     print(request.json)
 
-    return "file added to tracker, you are added as a host"
+    dummyResponse = {
+        "success" : True,
+        "file_id" : 42,
+        "guid" : "adsf54asd6f46asd54f65sd"
+    }
+
+    return jsonify(dummyResponse)
 
 
 # takes a json request with the clients guid as an argument
@@ -156,25 +225,29 @@ def add_file():
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "<true/false>"
+    "success" : <true/false>
 }
 '''
 # --- ON ERROR ---
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "false",
+    "success" : false,
     "error" : "<error reason>"
 }
 '''
-@app.route('/keep_alive' methods=['PUT'])
+@app.route('/keep_alive', methods=['PUT'])
 def keep_alive():
-    if(not request.json or not "title" in request.json):
+    if(not request.json):
         return "error: request is not json"
 
     print(request.json)
 
-    return "updated your ip as a host"
+    dummyResponse = {
+        "success" : True
+    }
+
+    return jsonify(dummyResponse)
 
 
 # takes a json req with the client's guid and the file id as args
@@ -183,7 +256,7 @@ def keep_alive():
 # Expects JSON blob in the form:
 '''
 {
-    "file_id" : "<file's id in the tracker db>",
+    "file_id" : <file's id in the tracker db>,
     "guid" : "<client's guid>"
 }
 '''
@@ -191,14 +264,14 @@ def keep_alive():
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "<true/false>"
+    "success" : <true/false>
 }
 '''
 # --- ON ERROR ---
 # Returns a JSON blob in the form:
 '''
 {
-    "success" : "false",
+    "success" : false,
     "error" : "<error reason>"
 }
 '''
